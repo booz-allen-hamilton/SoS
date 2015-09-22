47a9e6d18d1ba047132db25dfd8e7d0f535cafcd
unknown <579557@BAH5CG5151KXN.resource.ds.bah.com>
2015-09-22 18:05:01 -0400

    Added Term_Extractor files and example output.

--- First_Level_Code.py
+++ First_Level_Code.py
@@ -22,6 +22,7 @@ from xlrd import *
 from xlwt import *
 import collections
 import re
+from nltk.corpus import stopwords
 
 
 def read_first_sheet(file_name):
@@ -31,7 +32,7 @@ def read_first_sheet(file_name):
     :return: temp_sheet: text processed contents of first sheet. Row is system, column is term.
             surv_names: names of the surveillance systems
     """
-
+    stop = stopwords.words('english')
     current_book = open_workbook(file_name)
     active_sheet = current_book.sheet_by_index(0)
     temp_sheet_data = []
@@ -39,9 +40,11 @@ def read_first_sheet(file_name):
         temp_row = []
         for colNum in range(2, active_sheet.ncols):
             try:
-                temp_term = active_sheet.cell_value(rowNum,colNum).encode('ascii','ignore').lower().lstrip()
+                temp_term = active_sheet.cell_value(rowNum, colNum).encode('ascii', 'ignore').lower().strip()
                 if temp_term != "":
-                    temp_row.append(temp_term)
+                    if temp_term not in stop:
+                        if not temp_term.isdigit():
+                            temp_row.append(temp_term)
             except UnicodeEncodeError as e:
                 pass
         temp_sheet_data.append(temp_row)
@@ -88,29 +91,28 @@ if __name__ == "__main__":
 
             sheet_code,term_list = assign_first_code(sheet_data)
 
-        #Create output workbook and save data.
-        workbookOut = Workbook()
-        sheet1 = workbookOut.add_sheet("System Codes")
-        sheet2 = workbookOut.add_sheet("Code Legend")
+            #Create output workbook and save data.
+            workbookOut = Workbook()
+            sheet1 = workbookOut.add_sheet("System Codes")
+            sheet2 = workbookOut.add_sheet("Code Legend")
 
-        row = 0
-        for condition in term_list:
-            sheet2.write(row,0,condition)
-            sheet2.write(row,1,term_list[condition])
-            row+=1
-        workbookOut.save(doc_name[:len(doc_name)-5]+'.out.xls')
+            row = 0
+            for condition in term_list:
+                sheet2.write(row,0,condition)
+                sheet2.write(row,1,term_list[condition])
+                row+=1
 
-        row_n = 0
-        col = 1
-        for row in sheet_code:
-            sheet1.write(row_n, 0, surv_names[row_n])
-            for code in row:
-                sheet1.write(row_n, col, code)
-                col += 1
-            row_n += 1
+            row_n = 0
             col = 1
+            for row in sheet_code:
+                sheet1.write(row_n, 0, surv_names[row_n])
+                for code in row:
+                    sheet1.write(row_n, col, code)
+                    col += 1
+                row_n += 1
+                col = 1
 
-        workbookOut.save(doc_name[:len(doc_name)-5]+'.out.xls')
+            workbookOut.save(doc_name[:len(doc_name)-4]+'.out.xls')
 
     except SystemError as e:
         print e
--- Term_Extractor.py
+++ Term_Extractor.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """
-    Extracts 'important' terms from raw input for processing by First_Level_Code assignment.
-    Importance is determined by Part-Of-Speech Tagging via topia.termextract package.
+    Extracts information from master list, formats codes for first level processing by First_Level_Code.py.
 
     input: Arguments from the command line. First Argument is the file name. Next arguments are the names of the columns
     for processing.
@@ -14,6 +13,11 @@
 
 import sys
 from xlrd import *
+from topia.termextract import tag
+from topia.termextract import extract
+from nltk.corpus import stopwords
+from xlwt import *
+
 
 def get_sys_values(file_name):
     """
@@ -27,7 +31,12 @@ def get_sys_values(file_name):
     return sys_names
 
 def get_col_values(col_name,file_name):
-
+    """
+    Gets values for a desired column from the master file
+    :param col_name: column of interest
+    :param file_name: name of master file
+    :return: List of column data, if found. Nothing otherwise.
+    """
     current_book = open_workbook(file_name)
     active_sheet = current_book.sheet_by_index(0)
     row = active_sheet.row_values(0)
@@ -41,12 +50,64 @@ def get_col_values(col_name,file_name):
     print "Enter a valid column name!"
     return
 
+def extract_terms(phrase):
+    """
+    Initially intended to use NLP (POS recognition) for term extraction. However, as many terms are not
+    structured like text, better extraction is achieved by extracting all words and removing stop words with
+    NLTK.
+    :param phrase:
+    :return: list of non-stop words in phrase
+    """
+    stop = stopwords.words('english')
+    return [i for i in phrase.split() if i not in stop]
 
 #------------------------------------------------------
 
-if __name__== "__main__":
+if __name__ == "__main__":
     file_name = sys.argv[1]
     sys_values = get_sys_values(file_name)
     col_values = []
     for arg in sys.argv[2:]:
         col_values.append(get_col_values(arg,file_name))
+
+    processed_data = []
+    processed_single_col=[]
+    for single_col in col_values:
+        for single_phrase in single_col:
+            processed_single_col.append(extract_terms(single_phrase))
+        processed_data.append(processed_single_col)
+        processed_single_col = []
+
+    col_val_index = 0
+    for new_book_data in processed_data:
+        workbookOut = Workbook()
+        sheet1 = workbookOut.add_sheet("Extracted Terms")
+
+        row_c = 0
+        col_c = 2
+        for sys_terms in new_book_data[1:len(new_book_data)]:
+            sheet1.write(row_c, 0, sys_values[row_c+1])
+            for w in sys_terms:
+                sheet1.write(row_c, col_c, w)
+                col_c += 1
+            col_c = 2
+            row_c += 1
+
+        row_c = 0
+        for col in col_values[col_val_index][1:len(col_values[col_val_index])]:
+            sheet1.write(row_c, 1, col_values[col_val_index][row_c+1])
+            row_c += 1
+
+        workbookOut.save(file_name[:len(file_name)-5]+"."+new_book_data[0][0]+".xls")
+        col_val_index += 1
+
+
+
+
+
+
+
+
+
+
+
